e1eda310867f9e76bc0fce8cc76099cfa7dbd036
hbliu <hbliu@thoughtworks.com>
2017-03-08 12:00:13 +0800

    add fizzbuzz exercise

---
+++ Assignment/src/FizzBuzzExercise.java
@@ -0,0 +1,27 @@
+/**
+ * Created by hbliu.
+ */
+public class FizzBuzzExercise {
+    public static void main(String args[]) {
+        fizzBuzz();
+    }
+
+    private static void fizzBuzz() {
+        final int LOWEST_VALUE = 1;
+        final int HIGHEST_VALUE = 100;
+
+        for (int i = LOWEST_VALUE; i <= HIGHEST_VALUE; i++) {
+            boolean dividThree = i % 3 == 0;
+            boolean dividFive = i % 5 == 0;
+            if (dividThree && dividFive) {
+                System.out.println("FizzBuzz");
+            } else if (dividThree) {
+                System.out.println("Fizz");
+            } else if (dividFive) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(i);
+            }
+        }
+    }
+}
